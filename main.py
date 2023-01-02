2beafe3d1088823f3b1028ab03b0920b26bec877
a-burlakov <fln@mail.ru>
2023-01-02 19:15:47 +0500

    Testing for GitHub pages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from flask import Flask, render_template
 from flask_flatpages import FlatPages, pygments_style_defs
 from flask_frozen import Freezer
+import json
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -19,7 +20,11 @@ app.config.from_object(__name__)
 def index():
     posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
     posts.sort(key=lambda item: item['date'], reverse=True)
-    return render_template('index.html', posts=posts, bigheader=True)
+    with open('settings.json', encoding='utf8') as config:
+        data = config.read()
+        settings = json.loads(data)
+    return render_template('index.html', posts=posts, bigheader=True,
+                           **settings)
 
 
 @app.route('/posts/<name>/')
