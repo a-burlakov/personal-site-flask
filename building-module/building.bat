634eb021820f67934af28f9277f5ee53239fbedf
a-burlakov <fln@mail.ru>
2023-01-04 19:13:13 +0500

    Testing publication

--- building-module/building.bat
+++ building-module/building.bat
@@ -1,2 +1,3 @@
-
-cmd /k "cd /d C:\PythonProjects\personal-site\venv\Scripts & activate & cd /d    C:\PythonProjects\personal-site\building-module & python main.py build"
+@echo off
+cmd /C "cd /d C:\PythonProjects\personal-site\venv\Scripts & activate & cd /d    C:\PythonProjects\personal-site\building-module & python main.py build"
+exit
